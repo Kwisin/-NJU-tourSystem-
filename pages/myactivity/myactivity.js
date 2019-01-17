b7f2c50159dc8dc7fbbe441831206f673dcb1e30
Kwisin <45785198+Kwisin@users.noreply.github.com>
2019-01-17 19:17:35 +0800

    Add files via upload

---
+++ pages/myactivity/myactivity.js
@@ -0,0 +1,81 @@
+// pages/myactivity/myactivity.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  clickjoin:function(){
+    wx.navigateTo({
+      url: 'pages/activity-joined/activity-joined',
+    })
+  },
+  clickapply:function(){
+    wx.navigateTo({
+      url: 'pages/activity-applied/activity-applied',
+    })
+  },
+  clickexa:function(){
+    wx.navigateTo({
+      url: 'pages/activity-acked/activity-acked',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
