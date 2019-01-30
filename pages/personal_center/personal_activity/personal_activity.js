5d19cc0bbdb72f8b998541661d6fb9d4f181a5f4
finominon <zcj14@software.nju.edu.cn>
2019-01-30 17:11:42 +0800

    处理合并

---
+++ pages/personal_center/personal_activity/personal_activity.js
@@ -0,0 +1,79 @@
+// pages/personal_center/personal_activity/personal_activity.js
+
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    myactivity:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var userid = "1"
+    wx.request({
+      url: app.globalData.serverIP + "/activity/myActivityList/" + userid,
+      success: res => {
+        if (res.data.status == "ok") {
+          this.setData({
+            myactivity: res.data.data
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/personal_center/personal_info/personal_info.js
@@ -0,0 +1,88 @@
+// pages/personal_center/personal_info/personal_info.js
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    user: {},
+    userInfo: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+      var that = this;
+      var userid = "1";
+      wx.request({
+        url: app.globalData.serverIP + "/user/userinfo/"+userid,
+        method: 'GET',
+        header: {
+          "Content-Type": "application/json"
+        },
+        success: function (res) {
+          console.log("请求成功")
+          console.log(res.data.data)
+          that.setData({
+            user: res.data.data,
+            userInfo: app.globalData.userInfo
+          })
+        }
+      });
+    },
+
+
+  
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
